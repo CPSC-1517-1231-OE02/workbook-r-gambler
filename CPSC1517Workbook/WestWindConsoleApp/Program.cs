b651d7de73c8aadb8169adff983d8d53f306811d
Mark A <59680128+r-gambler@users.noreply.github.com>
2023-10-23 21:47:01 -0600

    db codes

---
+++ CPSC1517Workbook/WestWindConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using WestWindSystem.DAL;
+
+var db = new WestWindContext();
+
+var countCustomers = db.Customers.Count();
+
+Console.WriteLine($"There are {countCustomers} in db");
+
+var customer = db.Customers.FirstOrDefault();
+
+Console.WriteLine($"The first customer is {customer.ContactName}");
